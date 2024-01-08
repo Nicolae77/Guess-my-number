92bbf34492b7ff21d3c31b2b4c8fd8c3c8dfdac4
Nicolae Caldarasan <nicolaecaldarasan1983@gmail.com>
2024-01-08 11:15:18 +0000

    second commit

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 'use strict';
+// console.log(document.querySelector('.message').textContent);
+// console.log(document.querySelector('.between').textContent);
+// document.querySelector('.message').textContent = 'Correct Number! 🎉';
+// document.querySelector('.number').textContent = 13;
+// document.querySelector('.score').textContent = 10;
+// document.querySelector('.guess').value = 23;
+const my_func = function () {
+    const guess = Number(document.querySelector('.guess').value);
+    console.log(guess, typeof guess);
+};
+
+document.querySelector('.check').addEventListener('click', my_func);
