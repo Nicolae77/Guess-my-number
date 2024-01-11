9ede2edff213fcf195472ed15c9fe07b16afc2f5
Nicolae Caldarasan <nicolaecaldarasan1983@gmail.com>
2024-01-11 10:13:57 +0000

    Implementing highscores

--- script.js
+++ script.js
@@ -9,6 +9,7 @@
 // Generate a random number between 1 and 20
 const secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
+let highscore = 0;
 
 
 document.querySelector('.check').addEventListener('click', function () {
@@ -25,6 +26,11 @@ document.querySelector('.check').addEventListener('click', function () {
         document.querySelector('.number').textContent = secretNumber;
         document.querySelector('body').style.backgroundColor = '#60b347';
         document.querySelector('.number').style.width = '30rem';
+        // Highscore
+        if (score > highscore) {
+            highscore = score;
+            document.querySelector('.highscore').textContent = highscore;
+        }
         
         // When guuess is too high
     }else if (guess > secretNumber) {
